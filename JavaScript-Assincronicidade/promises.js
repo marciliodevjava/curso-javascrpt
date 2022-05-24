bc80f8b12d0c76f4de04b0623b24a62c76575dad
marciliodevjava <marciliodevjava@gmail.com>
2022-05-23 23:19:48 -0300

    Assicronicidade

---
+++ JavaScript-Assincronicidade/promises.js
@@ -0,0 +1,7 @@
+const myPromise = new ((resolve, reject) => {
+    window.setTimeout(() => {
+        resolve('Resolvida')
+    }, 2000);
+});
+
+await myPromise.then((result) => result + 'passando pelo then').then((result) => result + 'e agora acabou').catch((err) => console.log(err.message));
